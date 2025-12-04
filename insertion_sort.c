f44914836f1bc217efb2be463f1f2ba4027c5ebd
vuzquadeptrai <anhlunkkll@gmail.com>
2025-12-04 22:41:47 +0700

    no message

---
+++ insertion_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    if (scanf("%d", &n) != 1)
+        return 0;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (int i = 1; i < n; i++)
+    {
+        int key = a[i], j = i - 1;
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (i)
+            printf(" ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
